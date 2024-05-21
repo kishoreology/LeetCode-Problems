96abf962416d4e7b450f4e1ffdbec4cce49fc05c
kishore <106666554+kishoreology@users.noreply.github.com>
2024-05-21 11:38:27 +0530

    Time: 49 ms (6.33%) | Memory: 16.7 MB (30.01%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,25 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        Ans = []
-        n = len(nums)
+        L, ans = len(nums), list([[]])
 
-        # Helper function
-        def Helper(op, start_index):
-            # base case
-            if start_index == n:
-                Ans.append(list(op))
-                return
-            
-            # recursive case
-            # choice 1: include the current element
-            op.append(nums[start_index])
-            Helper(op, start_index + 1)
-            
-            # backtracking step
-            op.pop()
-            
-            # choice 2: exclude the current element
-            Helper(op, start_index + 1)
-        
-        Helper([], 0)
-        return Ans
+        mask = 2**L - 1
+        while mask:
+            copy, i, subset = mask, L - 1, list()
+            while copy:
+                if copy & 1: subset.append(nums[i])
+                i, copy = i - 1, copy >> 1
+
+            ans.append(subset)
+            mask -= 1
+
+        return ans
