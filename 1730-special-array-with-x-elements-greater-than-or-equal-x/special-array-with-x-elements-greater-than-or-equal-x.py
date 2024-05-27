78b98dd9ea0b905aa08382484030b20728c08e03
kishore <106666554+kishoreology@users.noreply.github.com>
2024-05-27 21:00:57 +0530

    Time: 46 ms (28.68%) | Memory: 16.5 MB (86.58%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
@@ -1,15 +1,13 @@
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
-        n: int = len(nums)
-        frequency: list[int] = [0 for _ in range(n + 1)]  # frequence[i] means there is frequence[i] elements equal to i
-
-        for num in nums:
-            frequency[min(n, num)] += 1
-
-        num_greater_equal = 0
-        for candidate_number in range(n, 0, -1):
-            num_greater_equal += frequency[candidate_number]
-            if candidate_number == num_greater_equal:
-                return candidate_number
-
+        nums.sort()
+        n = len(nums)
+    
+        for x in range(1, n + 1):
+            count = 0
+            for num in nums:
+                if num >= x:
+                    count += 1
+            if count == x:
+                return x
         return -1
