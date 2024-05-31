5c88097428ef1645adbac2522cca599680a33fe6
kishore <106666554+kishoreology@users.noreply.github.com>
2024-05-31 19:57:46 +0530

    Time: 47 ms (96.77%) | Memory: 18.6 MB (41.53%) - LeetSync

--- 260-single-number-iii/single-number-iii.py
+++ 260-single-number-iii/single-number-iii.py
@@ -1,19 +1,10 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
-        n: int = len(nums)
-        result: list[int] = [0, 0]
-        index = 0
-
-        for i in range(n):
-            found = False
-            for j in range(n):
-                if i != j and nums[i] == nums[j]:
-                    found = True
-                    break
-            if not found:
-                result[index] = nums[i]
-                index += 1
-                if index == 2:
-                    break
-
-        return result
+        ans = set()
+        for n in nums:
+            if n in ans:
+                ans.remove(n)
+            else:
+                ans.add(n)
+        return list(ans)
+        
